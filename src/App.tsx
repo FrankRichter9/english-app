63556836da265be0057dd767026b21daf1c25ad9
alsurin <alsurin@ozon.ru>
2023-02-12 23:16:52 +0300

    configured project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Logo, Menu } from './components'
+
+function App() {
+	return (
+		<div className="App">
+			<Logo />
+			<Menu status={'123'} />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Menu } from './menu'
+export { Logo } from './logo'
