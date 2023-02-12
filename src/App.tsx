5fb97aaf1203feddcdaae5afb98b3a81ab2ccd69
alsurin <alsurin@ozon.ru>
2023-02-12 23:40:03 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import { Logo, Menu } from './components'
+import { Login, Main } from './pages'
 
 function App() {
 	return (
 		<div className="App">
-			<Logo />
-			<Menu status={'123'} />
+			<Link to="/login">Login</Link>
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/login" element={<Login />} />
+				{/* <Route path="*" element={<Login />} /> */}
+			</Routes>
 		</div>
 	)
 }
---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './login.module.css'
+
+export class Login extends React.Component {
+	render() {
+		return (
+			<div className="root">
+				<h1>Login</h1>
+			</div>
+		)
+	}
+}
